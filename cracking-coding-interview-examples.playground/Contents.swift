89cdedad337db6ee576997e6cf9e8ce85d93886a
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-10-09 23:33:20 -0400

    first commit

---
+++ cracking-coding-interview-examples.playground/Contents.swift
@@ -0,0 +1,7 @@
+/*2. Check Permutation: Given two strings,write a method to decide if one is a permutation of the
+ other.
+ */
+
+func twoStringsPermutation(_ str1: String, _ str2: String) -> Bool {
+    
+}
